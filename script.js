7cb40a496c6c0c9108bd65d941bb781d0e58739a
d-swank <60488787+d-swank@users.noreply.github.com>
2023-03-24 14:16:07 -0700

    Put all files into the main folder

---
+++ script.js
@@ -0,0 +1,28 @@
+// var resumeButton = document.getElementById("resume-button");
+// var resumeViewer = document.getElementById("resume-pdf");
+// var resumeShown = false;
+
+
+// function toggleResume() {
+//     resumeViewer.style.width = 0;
+//     resumeViewer.style.height = 0;
+//     //resumeViewer.style.transition = 0.5 +"s";
+//     resumeViewer.style.visibility = "visible";
+
+//     if(!resumeShown)
+//     {
+//         resumeViewer.style.width = 900 + "px";
+//         resumeViewer.style.height = 1120 + "px";
+//         resumeButton.style.height = 20 + "px";
+//         resumeButton.innerText = "Close Resume";
+//         resumeShown = true;
+//     }
+//     else
+//     {
+//         resumeViewer.style.width = 0;
+//         resumeViewer.style.height = 0;
+//         resumeButton.style.height = 33 + "px";
+//         resumeButton.innerText = "Resume";
+//         resumeShown = false;
+//     }
+// }
