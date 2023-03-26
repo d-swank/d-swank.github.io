a3c81eecbec3d5258b1870b9934f15e0d75555e2
d-swank <60488787+d-swank@users.noreply.github.com>
2023-03-26 15:03:19 -0700

    Deleted unused code

--- script.js
+++ script.js
@@ -7,90 +7,16 @@ window.addEventListener('scroll', () => {
 
   if (currentScrollTop <= lastScrollTop) {
     navbarWrapper.classList.remove('scrolling');
-  } else {
+  } 
+
+  else {
     navbarWrapper.classList.add('scrolling');
   }
   lastScrollTop = currentScrollTop;
 });
 
-
 // Responisve Navbar
 function toggleMenu() {
-    var menu = document.querySelector(".right-side");
-    menu.classList.toggle("responsive");
-  }
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all the education items
-    var educationItems = document.querySelectorAll(".academics .portfolio-item-wrapper");
-
-    // Function to check if an element is in the viewport
-    function isInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-        return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.top <= windowHeight &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    // Function to apply the slide-in effect when an element is in the viewport
-    function applySlideIn() {
-        educationItems.forEach(function (item) {
-            if (isInViewport(item)) {
-                item.classList.add("visible");
-            }
-        });
-    }
-
-    // Call the function initially
-    applySlideIn();
-
-    // Call the function when the user scrolls
-    window.addEventListener("scroll", applySlideIn);
-});
-
-// Active nav links
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('.nav-link');
-    const sections = document.querySelectorAll('.content-wrapper');
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5
-    };
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const targetId = entry.target.id;
-          navLinks.forEach(link => {
-            const navLinkText = link.querySelector('.nav-link-text');
-            navLinkText.classList.remove('active');
-            if (link.getAttribute('href') === `#${targetId}`) {
-              navLinkText.classList.add('active');
-            }
-          });
-        }
-      });
-    }, options);
-  
-    sections.forEach(section => observer.observe(section));
-  
-    navLinks.forEach(link => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        navLinks.forEach(link => {
-          const navLinkText = link.querySelector('.nav-link-text');
-          navLinkText.classList.remove('active');
-        });
-        const navLinkText = e.target.closest('.nav-link').querySelector('.nav-link-text');
-        navLinkText.classList.add('active');
-        const target = document.querySelector(e.target.closest('.nav-link').getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-      });
-    });
-  });
+  var menu = document.querySelector(".right-side");
+  menu.classList.toggle("responsive");
+}
