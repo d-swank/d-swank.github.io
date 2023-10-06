ef88fc52356c3615d152e0320d8ee2ee6de35339
Dakota Swank <60488787+d-swank@users.noreply.github.com>
2023-10-06 00:28:27 -0700

    BIG Update
    Reorganized code and styled for mobile phones

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ window.addEventListener('scroll', () => {
   lastScrollTop = currentScrollTop;
 });
 
-// Responisve Navbar
+// Responisve Navbar for mobile (Hamburger menu)
 function toggleMenu() {
-  var menu = document.querySelector(".right-side");
+  var menu = document.querySelector(".nav-links-wrapper");
   menu.classList.toggle("responsive");
 }
 
-// SlideIn images effect
+// SlideIn images effect for Academics Section
 document.addEventListener("DOMContentLoaded", () => {
   const elements = [
     {
@@ -58,19 +58,19 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(element.el);
   });
 });
-
-// Darken background image effect
-const imageBkgrd = document.querySelectorAll('.item-wrapper-work');
   
-imageBkgrd.forEach(imageBkgrd => {
-  imageBkgrd.addEventListener('mouseover', () => {
-    imageBkgrd.childNodes[1].classList.add('img-darken');
+  // Darken background image effect
+  const imageBkgrd = document.querySelectorAll('.item-wrapper-work');
+    
+  imageBkgrd.forEach(imageBkgrd => {
+    imageBkgrd.addEventListener('mouseover', () => {
+      imageBkgrd.childNodes[1].classList.add('img-darken');
+    });
+  
+    imageBkgrd.addEventListener('mouseout', () => {
+      imageBkgrd.childNodes[1].classList.remove('img-darken');
+    });
   });
-
-  imageBkgrd.addEventListener('mouseout', () => {
-    imageBkgrd.childNodes[1].classList.remove('img-darken');
-  });
-});
 
 // Tab Menu Functionality
 const tabs = document.querySelectorAll('.tab-menu-item');
@@ -103,3 +103,27 @@ const initialTab = tabs[0];
 const line = document.querySelector('.line');
 line.style.left = `${initialTab.offsetLeft}px`;
 line.style.width = `${initialTab.offsetWidth}px`;
+
+// SlideIn images effect for Work Section
+window.addEventListener("scroll", function() {
+  const section = document.querySelector('.item-wrapper-work');
+  const textBox = document.querySelector('.item-info-work');
+  const rect = section.getBoundingClientRect();
+  const maxSlideUp = textBox.offsetHeight;  // Maximum slide distance should be the height of the text box.
+
+  // When the image is fully in the viewpoint, start the slide-up effect
+  if(rect.top <= window.innerHeight && rect.bottom >= textBox.offsetHeight) {
+      // Calculate the amount the text box should move up by
+      const moveUpBy = ((window.innerHeight - rect.top) / window.innerHeight) * maxSlideUp; 
+      const newTop = (moveUpBy / section.offsetHeight * 100) - 100;
+
+      // Adjust the top position of the text box
+      if (newTop > -32 && newTop < 0) {  // Check if the new position is between -32% and 0
+          textBox.style.top = `${newTop}%`; 
+      } else if (newTop <= -32) {  // If the calculated position is above -32%, then stop at -32%
+          textBox.style.top = `3%`;  
+      } else if (newTop >= 0) { 
+          textBox.style.top = '0%';  // Reset to the initial position if the calculation returns a value greater than or equal to 0
+      }
+  }
+});
